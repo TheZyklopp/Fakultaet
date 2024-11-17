e3685cf132eff3037274e17abc99527b4ab4ec45
TheZyklopp <Philfaber1999@gmail.com>
2024-11-17 15:42:36 +0100

    Projektdateien hinzufügen.

---
+++ Fakultaet/Program.cs
@@ -0,0 +1,64 @@
+﻿using System.Numerics; //brauch man durch BigInt
+
+namespace Fakultaet
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            uint number = 21;
+            
+            try
+            {
+                ulong ergebniss = CalculateFactorial(number); //max ist ulong
+                Console.WriteLine($"Die Fakultät von {number} ist {ergebniss}");
+            }
+            catch (OverflowException) //wenn ein Overflow auftritt wird dieser Bereich ausgeführt.
+            {
+                Console.WriteLine("Ein Overflow ist aufgetreten.");
+            }
+
+            // Testen der CalculateBigFactorial Methode
+            uint bigNumber = 50;
+            try
+            {
+                BigInteger bigErgebniss = CalculateBigFactorial(bigNumber);
+                Console.WriteLine($"Die Fakultät von {bigNumber} ist {bigErgebniss}");
+            }
+            catch (OverflowException) //wenn ein Overflow auftritt wird dieser Bereich ausgeführt.
+            {
+                Console.WriteLine("Ein Overflow ist aufgetreten.");
+            }
+        }
+
+        //Berechnet die Fakultät bis 20
+        public static ulong CalculateFactorial(uint number)
+        {
+            ulong result = 1;
+
+            checked //Prüft nach Overflow
+            {
+                for (ulong i = 1; i <= number; i++) //rechnet immer das ergebnis * i++
+                {
+                    result *= i;
+                }
+            }
+            return (result);
+        }
+
+        //Berechnet die Fakultät
+        public static BigInteger CalculateBigFactorial(uint number)
+        {
+            BigInteger result = 1;
+
+            checked //Prüft nach Overflow
+            {
+                for (uint i = 1; i <= number; i++) //rechnet immer das ergebnis * i++
+                {
+                    result *= i;
+                }
+            }
+            return result;
+        }
+    }
+}
